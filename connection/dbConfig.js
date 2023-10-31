5b4c6f1adcb659a453e221391268bff3a74c0193
Gaurav Kothari <87779512+kotharigaurav6@users.noreply.github.com>
2023-10-31 19:02:36 +0530

    Add files via upload

--- connection/dbConfig.js
+++ connection/dbConfig.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 //mongoose.connect("mongodb://127.0.0.1:27017/facultyrecruitmentitep5");
- mongoose.connect("mongodb+srv://kotharigaurav6:gaurav_kothari1992@cluster0.kiqyksu.mongodb.net/faculty_recruitment_system?retryWrites=true&w=majority");
+ mongoose.connect("mongodb+srv://kotharigaurav6:gaurav_kothari1992@cluster0.m0bpqsg.mongodb.net/FacultyRecruitmentSystem?retryWrites=true&w=majority");
 
 export default mongoose;
